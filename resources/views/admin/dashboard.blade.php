eb587d97fbbea68d81cbb2fb1f3e4c119b34cc71
Shady <shady.ber9@gmail.com>
2022-01-26 20:52:30 +0300

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,7 +13,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$ {{\App\Models\Payment::paymentsAmountByDate(strtotime("-30 days"),today())}}</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$  </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -31,7 +31,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$$ {{\App\Models\Payment::paymentsAmountByDate(strtotime("-365 days"),today())}}</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$$  </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>

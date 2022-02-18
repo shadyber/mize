c51d8a5b9c82b4e34c675f1848c1f12b2e827e5e
Shady <shady.ber9@gmail.com>
2022-02-18 23:13:43 +0300

    user repoutation
    user reputation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,10 @@
 
 @section('content')
 
-<div class="container">
+<div class="row p-2 m-4">
 
-        <div class="row">
             <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
-                <div class="card card-profile shadow">
+                <div class="card card-profile ">
                     <div class="row justify-content-center">
                         <div class="col-lg-3 order-lg-2">
                             <div class="card-profile-image">
@@ -18,8 +17,7 @@
                     </div>
                     <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
                         <div class="d-flex justify-content-between">
-                            <a href="#" class="btn btn-sm btn-info mr-4">Connect</a>
-                            <a href="#" class="btn btn-sm btn-default float-right">Message</a>
+
                         </div>
                     </div>
                     <div class="card-body pt-0 pt-md-4">
@@ -27,43 +25,36 @@
                             <div class="col">
                                 <div class="card-profile-stats d-flex justify-content-center mt-md-5">
                                     <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">Friends</span>
+                                        <span class="heading"> {{count(Auth::user()->orders)}} </span>
+                                        <span class="description">Ordres</span>
                                     </div>
-                                    <div>
-                                        <span class="heading">10</span>
-                                        <span class="description">Photos</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">Comments</span>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
                         <div class="text-center">
                             <h3>
-                                Jessica Jones<span class="font-weight-light">, 27</span>
+                                {{Auth::user()->name}}<span class="font-weight-light">, 0 <span class="ui-icon-star"></span></span>
                             </h3>
                             <div class="h5 font-weight-300">
-                                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+                                <i class="ni location_pin mr-2"></i>***{{(Auth::user()->email)}}
                             </div>
                             <div class="h5 mt-4">
-                                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+                                <i class="ni business_briefcase-24 mr-2"></i>
                             </div>
                             <div>
-                                <i class="ni education_hat mr-2"></i>University of Computer Science
+                                <i class="ni education_hat mr-2"></i>Registered {{Auth::user()->created_at->diffForHumans()}}
                             </div>
                             <hr class="my-4">
-                            <p>Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
+                            <p>User Reputation.</p>
                             <a href="#">Show more</a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-8 order-xl-1">
-                <div class="card bg-secondary shadow">
-                    <div class="card-header bg-white border-0">
+                <div class="card ">
+                    <div class="card-header">
                         <div class="row align-items-center">
                             <div class="col-8">
                                 <h3 class="mb-0">My account</h3>
@@ -74,7 +65,7 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <form>
+
                             <h6 class="heading-small text-muted mb-4">User information</h6>
                             <div class="pl-lg-4">
                                 <div class="row">
@@ -106,59 +97,98 @@
                                     </div>
                                 </div>
                             </div>
-                            <hr class="my-4">
-                            <!-- Address -->
-                            <h6 class="heading-small text-muted mb-4">Contact information</h6>
-                            <div class="pl-lg-4">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-address">Address</label>
-                                            <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-city">City</label>
-                                            <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-country">Country</label>
-                                            <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label class="form-control-label" for="input-country">Postal code</label>
-                                            <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
                             <hr class="my-4">
                             <!-- Description -->
-                            <h6 class="heading-small text-muted mb-4">About me</h6>
+                            <h6 class="heading-small text-muted mb-4">My Orders</h6>
                             <div class="pl-lg-4">
                                 <div class="form-group focused">
-                                    <label>About Me</label>
-                                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+                                    <label>My Orders</label>
+                                    <div class="row">
+                                        @foreach( \Illuminate\Support\Facades\Auth::user()->orders as $order)
+                                            <div class="box card p-3 m-5 col-md-6">
+                                                <div class="card-header">  {{$order->address->city}} : {{$order->address->postal_code}} </div>
+                                                <div class="card-body">
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <p>Shipping Method: </p>
+                                                        </div>
+                                                    </div>
+                                                    <ul>
+
+                                                        @foreach($cart_item=json_decode($order->cart) as $cart)
+
+                                                            <li class="btn btn-outline-primary pull-right"><a href="/item/{{$cart->slug}}" class="list-inline-item">{{$cart->name}}</a>  X {{$cart->quantity}}  </li>
+                                                        @endforeach
+
+                                                    </ul>
+                                                </div>
+                                                <div class="card-footer">
+
+                                                    <span class="ui-checkboxradio-label"> Status {{$order->status}} </span>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+
+
+
                                 </div>
                             </div>
-                        </form>
+
+                            <hr class="my-4">
+                            <!-- Address -->
+                            <h6 class="heading-small text-muted mb-4">Shipping Informations</h6>
+                            <div class="row">
+
+                                    <h3>My Shipping Address</h3>
+                                    @Auth
+                                        @foreach( \Illuminate\Support\Facades\Auth::user()->ShippinInfo as $address)
+                                            <div class="box card p-3 m-5 col-md-4">
+                                                <div class="card-header"> {{$address->country_code}} : {{$address->city}} </div>
+                                                <div class="card-body">
+                                                    ST: {{$address->street_address}}<br>
+                                                    POB: {{$address->postal_code}}<br>
+                                                    Tel: {{$address->phone}}<br>
+                                                </div>
+                                                <div class="card-footer">
+
+                                                    <input type="radio" class="checkbox checkbox-" name="shpping_info_id" value="{{$address->id}}"  onclick="change(this.value);">
+                                                    <span class="ui-checkboxradio-label">Use This As Primary Address </span>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                        <div class="box card pb-50 m-5">
+                                            <div class="card-header"> Create New Shpping Address</div>
+                                            <div class="card-body">
+                                                <a href="#" class="btn btn-lg btn-block btn-outline-info"  data-toggle="modal" data-target="#exampleModalCenter"> <span class="icofont-plus "></span></a>
+
+
+                                            </div>
+
+                                        </div>
+
+
+
+
+                                    @endauth
+
+
+                            </div>
+
+
                     </div>
                 </div>
             </div>
-        </div>
+
 
 </div>
 
 
+@include('components.fronmodal')
 @endsection
+
+
 @section('css')
-    <link href="/css/homepage.css" rel="stylesheet">
 
 @endsection
--- resources/views/layouts/inc/header.blade.php
+++ resources/views/layouts/inc/header.blade.php
@@ -30,6 +30,96 @@
 
                     </li>
                     <li><a href="/">Contact</a></li>
+
+                    <li>
+
+                        <a  href="#" >
+                            <span class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
+                        </a>
+                        <ul class="sub-menu pb-10 pt-10 pl-15 pr-15">
+                            @foreach (Config::get('languages') as $lang => $language)
+                                @if ($lang != App::getLocale())
+                                    <li> <a  href="{{ route('lang.switch', $lang) }}"><span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span> {{$language['display']}}</a></li>
+                                @endif
+                            @endforeach
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a  href="#" >
+                            <span class="fa fa-user"></span> {{\Illuminate\Support\Facades\Auth::user()? \Illuminate\Support\Facades\Auth::user()->name : 'Guest'}}
+                        </a>
+                        <ul class="sub-menu pb-10 pt-10 pl-15 pr-15">
+                            @guest()
+                                <li> <a  href="/login"><span class="fa fa-user"></span> Login</a></li>
+                                <li> <a  href="/register"><span class="fa fa-fill"></span> Register</a></li>
+                            @endguest
+                            @auth()
+                                <li> <a  href="/home"><span class="fa fa-user"></span> Profile</a></li>
+                                <li>
+                                    <a  href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();"> <span class="fa fa-power-off"></span>
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+
+                                </li>
+                            @endauth
+
+                        </ul>
+
+                    </li>
+
+
+                </ul>
+
+                <ul class="lab-ul search-cart">
+                    <li>
+                        <div class="cart-option">
+                            <i class="icofont-cart-alt"></i>
+                            <span id="cartCount">{{count(App\Models\Cart::mycart())}}</span>
+                            <div class="cart-content">
+                                @foreach(\App\Models\Cart::myCart() as $cart)
+                                    <div class="cart-item">
+                                        <div class="cart-img">
+                                            <a href="#"><img src="{{$cart['thumb']}}"
+                                                             alt="cart"></a>
+                                        </div>
+                                        <div class="cart-des">
+                                            <a href="#">{{$cart['name']}}</a>
+                                            <p> {{$cart['quantity']}} X ${{$cart['price']}}</p>
+                                        </div>
+                                        <div class="cart-btn">
+                                            <a href="#"><i class="icofont-close-circled"></i></a>
+                                        </div>
+                                    </div>
+                                @endforeach
+
+
+                                <div class="cart-bottom">
+                                    <div class="cart-subtotal">
+                                        <p>Total: <b class="float-right" id="total">${{\App\Models\Cart::totalCart()}}</b></p>
+                                    </div>
+                                    <div class="cart-action">
+                                        <a href="/mycart" class="lab-btn"><span>View Cart</span></a>
+                                        <a href="/checkout?shipping_method_id=1" class="lab-btn"><span>Check Out</span></a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="search-option">
+                            <i class="icofont-search-2"></i>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
